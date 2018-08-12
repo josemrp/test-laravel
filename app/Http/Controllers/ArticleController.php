941537b1426edc50f7b0dff649700ab29b75823e
josemrp <josemanuelromerop@gmail.com>
2018-08-12 09:11:58 -0400

    Validation

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\StoreArticle;
 use App\Article;
 use App\Tag;
 
@@ -38,10 +39,10 @@ class ArticleController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  StoreArticle  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreArticle $request)
     {
         //Insert Article
         $article = new Article();
